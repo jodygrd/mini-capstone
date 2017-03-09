efa9b5fc1370f301477ef784c7504eb4559e266f
Jody Lipkin <lipkinjody@gmail.com>
2017-03-08 17:53:10 -0800

    page to display all produce

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,7 @@
 class ProductsController < ApplicationController
+
+	def display_all_products
+		@products = Produce.all
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+
+	get "/display_all_products" => "products#display_all_products"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
