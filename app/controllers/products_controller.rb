d28c1a2c9de73530a885a61652e81ef3d9090587
Jody Lipkin <lipkinjody@gmail.com>
2017-03-09 21:17:52 -0800

    new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,18 @@ class ProductsController < ApplicationController
 		@products = Produce.all
 	end
 
+	def add_product
+	end
+
+	def product_added
+
+		name_input = params[:name]
+		price_input = params[:price]
+		description_input = params[:description]
+		image_input = params[:image]
+
+    @produce = Produce.create({name: name_input, price: price_input, description: description_input, image: image_input})
+
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
 	get "/display_all_products" => "products#display_all_products"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/add_product" => "products#add_product"
+  post "/product_added" => "products#product_added"
+
+
+
 end
